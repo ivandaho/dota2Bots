7a4aed693d12080b839a683b5b5ae9aea595bfc0
ivandaho <ivandaho@gmail.com>
2016-12-27 02:32:15 -0500

    Add primitive storm_spirit bot
    Bot will patrol the lane and last hit
    
    Also Make bots use courier if it exists in their inventory

--- ability_item_usage_generic.lua
+++ ability_item_usage_generic.lua
@@ -1,4 +1,4 @@
-
+local DotaBotUtility = require(GetScriptDirectory().."/utility");
 
 ----------------------------------------------------------------------------------------------------
 
@@ -8,6 +8,11 @@ module( "ability_item_usage_generic", package.seeall )
 ----------------------------------------------------------------------------------------------------
 
 function AbilityUsageThink()
+	local courier = DotaBotUtility.IsItemAvailable("item_courier");
+	if(courier ~= nil) then
+		local npcBot = GetBot();
+		npcBot:Action_UseAbility(courier);
+	end
 
 	--print( "Generic.AbilityUsageThink" );
 
---
+++ mode_rune_storm_spirit.lua
@@ -0,0 +1,19 @@
+function OnStart()
+	-- Do the standard OnStart
+	print("storm - starting to do rune stuff");
+end
+
+----------------------------------------------------------------------------------------------------
+
+function OnEnd()
+	-- Do the standard OnEnd
+	print("storm - ending rune stuff");
+end
+
+----------------------------------------------------------------------------------------------------
+
+function Think()
+end
+
+function GetDesire()
+end
